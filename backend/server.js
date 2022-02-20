eda40f985151f4bad840641bd7eae89bc0ceb051
ahmed <ahmedlearn01@gmail.com>
2022-02-19 17:12:08 -0800

    crud done

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 
 const PORT = process.env.Port || 5000;
-
 const app = express();
+// middlewares
+app.use('/api/goals',require('./goalRoutes/getGoalRoutes'));
 
 app.listen(PORT, ()=>console.log(`server is running ${PORT}`));
