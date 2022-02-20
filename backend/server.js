369941c2456bc440782df18f9242929c1cf1b5f2
ahmed <ahmedlearn01@gmail.com>
2022-02-19 16:47:53 -0800

    server is runnign

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log('hellooo world')
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const PORT = process.env.Port || 5000;
+
+const app = express();
+
+app.listen(PORT, ()=>console.log(`server is running ${PORT}`));
