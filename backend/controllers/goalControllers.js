b2b7b40401a8d394c68f8cf18c8ae2015e7ab952
ahmed <ahmedlearn01@gmail.com>
2022-02-26 11:34:23 -0800

    mongo db atlas

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,13 +1,16 @@
+const asyncHandler = require('express-async-handler');
+
+
 // @desc : GET goals
 // @route : GET /api/goals
 // @access : Private
-const getGoals = (req,res)=>{
+const getGoals = asyncHandler(async (req,res)=>{
     res.status(200).json({ message: 'get goals' });
-}
+})
 // @desc : Set goals
 // @route : POST /api/goals
 // @access : Private
-const setGoal = (req,res)=>{
+const setGoal =asyncHandler(async (req,res)=>{
     
     if(!req.body.text){
         res.status(400) //.json({message: 'please fill in the text'})
@@ -15,20 +18,19 @@ const setGoal = (req,res)=>{
     }
 
     res.status(200).json({ message: 'Create goals' });
-}
+})
 // @desc : UPDATE goals
 // @route : PUT /api/goals/:id
 // @access : Private
-const updateGoal = (req,res)=>{
+const updateGoal =asyncHandler(async (req,res)=>{
     res.status(200).json({ message: `update Goal ${req.params.id}` });
-}
+})
 // @desc : detele goals
 // @route : DELETE /api/goals/:id
 // @access : Private
-const deleteGoal = (req,res)=>{
+const deleteGoal =asyncHandler(async (req,res)=>{
     res.status(200).json({ message: `delete Goal ${req.params.id}` });
-}
-
+})
 
 module.exports = {
     getGoals,
