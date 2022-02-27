3fd788828f8b10bcb91751df70427694a31f4130
ahmed <ahmedlearn01@gmail.com>
2022-02-26 17:10:05 -0800

    get goals

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,11 +1,14 @@
 const asyncHandler = require('express-async-handler');
 
+const goal = require('../models/goalModel');
 
 // @desc : GET goals
 // @route : GET /api/goals
 // @access : Private
 const getGoals = asyncHandler(async (req,res)=>{
-    res.status(200).json({ message: 'get goals' });
+    const goals = await goal.find();
+
+    res.status(200).json(goals);
 })
 // @desc : Set goals
 // @route : POST /api/goals
---
+++ backend/models/goalModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const goalSchema = mongoose.Schema({
+    text:{
+        type: String,
+        required: [true, 'please enter text field']
+    }
+},{
+    timestamps: true
+}
+)
+
+module.exports = mongoose.model('Goal',goalSchema);
