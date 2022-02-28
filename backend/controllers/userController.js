f0ad00d5fd086b657fa7ced4900b246242a86d8a
ahmed <ahmedlearn01@gmail.com>
2022-02-27 18:19:27 -0800

    users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,25 @@
+// @desc  Register user
+// @api   POST api/users
+// @access  public
 const registerUser = (req,res)=>{
     res.json({message: 'register user'})
+}
+// @desc  login user
+// @api   POST api/users/login
+// @access  public
+const loginUser = (req,res)=>{
+    res.json({message: 'Login user'})
+}
+
+// @desc  Get user Data
+// @api   GET api/users/me
+// @access  public
+const getMe = (req,res)=>{
+    res.json({message: 'Get my Data'})
 }
 
 module.exports = {
     registerUser,
+    loginUser,
+    getMe
 }
