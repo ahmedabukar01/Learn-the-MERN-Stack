a90a5adba018a3713fb517ce7f2d2b53afaa37b2
ahmed <ahmedlearn01@gmail.com>
2022-03-16 17:44:21 -0700

    register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import {FaUser} from 'react-icons/fa';
+import {useState, useEffect } from 'react';
 
 function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const {name, email, password, password2} = formData;
+
+  const onChange = ()=>{
+    
+  }
+
   return (
-    <div>Register</div>
+    <>
+    <section className="heading">
+      <h1> <FaUser /> Register</h1>
+      <p>Please Create an account</p>
+    </section>
+
+    <section className="form">
+      <form>
+        <div className="form-group">
+          <input type="text" className="form-control" 
+            id="name" name="name" value={name}
+            placeholder="enter your name" onChange={onChange}/>
+        </div>
+      </form>
+    </section>
+    </>
   )
 }
 
