24333404cf16ecd2fecff314f45cffe27e8f297f
ahmed <ahmedlearn01@gmail.com>
2022-03-16 17:53:06 -0700

    register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,8 +11,14 @@ function Register() {
 
   const {name, email, password, password2} = formData;
 
-  const onChange = ()=>{
-    
+  const onChange = (e)=>{
+    setFormData((prevState)=>({
+      ...prevState, [e.target.name]: e.target.value,
+    }))
+
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
   }
 
   return (
@@ -23,11 +29,29 @@ function Register() {
     </section>
 
     <section className="form">
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <input type="text" className="form-control" 
             id="name" name="name" value={name}
             placeholder="enter your name" onChange={onChange}/>
+        </div>
+        <div className="form-group">
+          <input type="email" className="form-control" 
+            id="email" name="email" value={email}
+            placeholder="enter your email" onChange={onChange}/>
+        </div>
+        <div className="form-group">
+          <input type="password" className="form-control" 
+            id="password" name="password" value={password}
+            placeholder="enter your password" onChange={onChange}/>
+        </div>
+        <div className="form-group">
+          <input type="password" className="form-control" 
+            id="password2" name="password2" value={password2}
+            placeholder="Comfirm your password" onChange={onChange}/>
+        </div>
+        <div className="form-group">
+          <button type="submit" className='btn btn-block'>Submit</button>
         </div>
       </form>
     </section>
