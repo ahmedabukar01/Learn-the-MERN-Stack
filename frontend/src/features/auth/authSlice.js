a82976b742c2cc64c63c3403d661910aab98afb1
ahmed <ahmedlearn01@gmail.com>
2022-03-19 17:02:50 -0700

    redux

---
+++ frontend/src/features/auth/authSlice.js
@@ -0,0 +1,29 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import { FaGlasses } from 'react-icons/fa';
+
+// get user from localStorage
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+    user: user ? user : null,
+    isError: false,
+    isLoading: false,
+    isSuccess: false,
+    message: ''
+}
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        reset: () => {
+            state.isLoading = false
+            state.isError = false
+            state.isSuccess = false
+            state.message = ''
+        }
+    },
+    extraReducers : ()=> {}
+});
+export const { reset } = authSlice.actions
+export default authSlice.reducer
